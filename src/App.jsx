58eeffed889a0183762a144264fb918fd04b7358
brianvikonura <vikonura@gmail.com>
2025-03-06 11:34:47 +0700

    finish product detail and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faDog } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   const [count, setCount] = useState(0)
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,17 @@ import "./index.css";
 import App from "./App.jsx";
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter, Route, Routes } from "react-router";
+import ProductDetail from "./views/DetailProduk.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Toaster />
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
